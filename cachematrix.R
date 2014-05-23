e23c074125af34b70304cd2184df9799457eaa62
lmendoza <leemendoza@gmail.com>
2014-05-22 22:44:32 -0500

    update to programming assignment 2
    completed bodies of stub functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix : function that caches the inverse
+## of a supplied matrix to save the computing expense
+## of recalcuating every time the inverse is needed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## see if supplied matrix is different from earlier matrix
+    ## or if an inverse does not exist
+    if ((x != gx) || !exists("invx"))    {
+        ## Gotta invert the matrix - go ahead and cache it too
+        invx <<- solve(x)
+        ## cache the matrix as well for future comparison
+        gx <<- x
+    }
+    else {
+        ## Use cached matrix inverse instead
+    }
+    
+    ## return inverse of matrix, cached or otherwise
+    invx
+    
+    ## don't show the output
+    invisible()
+        
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve:
+## computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## makeCacheMatrix does all the work, we just 
+    ## call it here...
+    m = makeCacheMatrix(x)
+        
+    ## Return a matrix that is the inverse of 'x' calculated
+    ## or retrieved from the cache in makeCacheMatrix
+    m
+    
+    ## hide the output
+    invisible()
 }
