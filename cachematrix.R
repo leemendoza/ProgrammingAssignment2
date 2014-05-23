32427cd05fe203b5979dc33b14296d09049dd27f
lmendoza <leemendoza@gmail.com>
2014-05-23 06:35:44 -0500

    updated and tested code
    added function equalMatrices to test matrix equality - if matrix passed
    to makeCacheMatrix is not equal to cached matrix, recalculate inverse
    and cache inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,19 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## equalMatrices : function that tests equality between two
+## matrices. We'll use this as one test to determine if we
+## can use a cached version of the inverted matrix in makeCacheMatrix
+
+equalMatrices <- function(a, b){
+
+    ## check to see if a and b are matrices
+    ## check to see if a and b have same dimensions
+    ## check to see if a and b elements are same
+    res = is.matrix(a) && is.matrix(b) && dim(a) == dim(b) && all(a == b)
+    res
+}
+
 ## makeCacheMatrix : function that caches the inverse
 ## of a supplied matrix to save the computing expense
 ## of recalcuating every time the inverse is needed
@@ -8,14 +21,16 @@
 makeCacheMatrix <- function(x = matrix()) {
     ## see if supplied matrix is different from earlier matrix
     ## or if an inverse does not exist
-    if ((x != gx) || !exists("invx"))    {
+    if (!exists("gx") || !equalMatrices(x, gx) || !exists("invx"))    {
         ## Gotta invert the matrix - go ahead and cache it too
+        ## print("Solving...")
         invx <<- solve(x)
         ## cache the matrix as well for future comparison
         gx <<- x
     }
     else {
         ## Use cached matrix inverse instead
+        ## print("Using cached version")
     }
     
     ## return inverse of matrix, cached or otherwise
